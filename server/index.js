9a2cd0c7ee676dc2cec0a340ab0c3437a6b31ee1
Phil Nash <philnash@gmail.com>
2017-09-11 16:53:26 +0100

    Removes old examples folder from server

--- server/index.js
+++ server/index.js
@@ -20,15 +20,6 @@ var randomName = require('./randomname');
 // Create Express webapp.
 var app = express();
 
-// Set up the paths for the examples.
-[
-  'mediadevices',
-  'localvideosnapshot'
-].forEach(function(example) {
-  var examplePath = path.join(__dirname, `../examples/${example}/public`);
-  app.use(`/${example}`, express.static(examplePath));
-});
-
 // Set up the path for the quickstart.
 var quickstartPath = path.join(__dirname, '../quickstart/public');
 app.use('/quickstart', express.static(quickstartPath));
